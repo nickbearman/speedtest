bf22f32976393ef31def7a611fa915b8fda6d40f
Nick Bearman <nick@nickbearman.me.uk>
2016-12-17 22:21:07 +0000

    Revised speedtest code

--- speedtest.sh
+++ speedtest.sh
@@ -1,14 +1,6 @@
-#original speed test
+#speed test using wget
 wget --output-document=/dev/null --report-speed=bits http://speedtest.wdc01.softlayer.com/downloads/test10.zip 2>&1 | grep ') -' >> /home/pi/speedtest/speedtest-results.txt
 
-#new speedtest 
-#date >> speedtest-results.txt
-#speedtest-cli --simple >> speedtest-results.txt
-
-#speedtest script
-#speedtest-cli-extras/bin/speedtest-csv >> speedtest-results.csv
-#echo "" >> speedtest-results.csv
-
-#run R script
+#run R script to output summary.txt
 Rscript ~/speedtest/script.R
 
