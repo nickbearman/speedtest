5698d56e7bdb662f2d0890b183ac5f802d2e8fad
Nick Bearman <nick@nickbearman.me.uk>
2016-12-17 22:22:19 +0000

    Upload speedtest to Dropbox

---
+++ speedtest-upload.sh
@@ -0,0 +1,5 @@
+/home/pi/Dropbox-Uploader/dropbox_uploader.sh upload /home/pi/speedtest/speedtest-results.txt speedtest-results.txt
+
+/home/pi/Dropbox-Uploader/dropbox_uploader.sh upload /home/pi/speedtest/summary.txt summary/summary.txt
+
+/home/pi/Dropbox-Uploader/dropbox_uploader.sh upload /home/pi/speedtest/speedtest-results.csv speedtest-results.csv
