2441fe7c2b98384b7fdb6f109984b90c6df99a80
Nick Bearman <nick@nickbearman.me.uk>
2017-01-06 18:01:41 +0000

    Updates to correct file paths

--- speedtest.sh
+++ speedtest.sh
@@ -2,5 +2,5 @@
 wget --output-document=/dev/null --report-speed=bits http://speedtest.wdc01.softlayer.com/downloads/test10.zip 2>&1 | grep ') -' >> /home/pi/speedtest/speedtest-results.txt
 
 #run R script to output summary.txt
-Rscript ~/speedtest/script.R
+Rscript /home/pi/speedtest/script.R
 
