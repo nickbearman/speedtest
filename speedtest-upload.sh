e6285cf350569103c6521486a48bb344264fd0b0
Nick Bearman <nick@nickbearman.me.uk>
2017-12-16 09:25:18 +0000

    Correct file paths

--- speedtest-upload.sh
+++ speedtest-upload.sh
@@ -1,4 +1,4 @@
 /home/pi/Dropbox-Uploader/dropbox_uploader.sh upload /home/pi/speedtest/speedtest-results.txt speedtest-results.txt
 
-/home/pi/Dropbox-Uploader/dropbox_uploader.sh upload /home/pi/speedtest/summary.txt summary/summary.txt
+/home/pi/Dropbox-Uploader/dropbox_uploader.sh upload /home/pi/speedtest/summary.txt summary.txt
 
